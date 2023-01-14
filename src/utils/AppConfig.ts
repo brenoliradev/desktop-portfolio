aa55a9318ddbe01b63d940ba7784edc1a5b0a92e
lirbre <brenoliradev@gmail.com>
2023-01-14 13:12:19 -0300

    feat: implements project configuration to next head

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Breno Lira - Portfolio',
+  title: 'Breno Lira - Portfolio',
+  description: 'A portfolio that resembles an desktop.',
   locale: 'en'
 }
